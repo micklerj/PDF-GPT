8208bb6ea011f6eaf7a3d53fd4803a4629cd980f
Noah White <noah.white@ufl.edu>
2024-04-10 12:45:32 -0400

    Initial frontend interface commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,54 @@
 import logo from './logo.svg';
+import './normal.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+    <aside className = "sidemenu">
+      <div className="side-menu-button">
+        <span>+</span>
+        New Chat
+      </div>
+    </aside>  
+    <section className = "chatbox">
+      <div className="chat-log">
+        <div className="chat-message">
+          <div className="chat-message-center">
+            <div className="avatar">
+            
+            </div>
+            <div className="message">
+              Hello World
+            </div>
+          </div>
+        </div>
+        <div className="chat-message chatbot">
+          <div className="chat-message-center">
+            <div className="avatar chatbot">
+            
+            </div>
+            <div className="message">
+              I am a chatbot
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className = "chat-input-holder">
+        <textarea
+        rows="1"
+        className="chat-input-textArea">
+        </textarea>
+      </div>
+    </section>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +62,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+*/
