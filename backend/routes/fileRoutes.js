ffcc9c4ccc2985b16e66e02d13e7e914305a4c57
adnan-farid <faridadnan810@gmail.com>
2024-04-21 16:18:35 -0400

    nit

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -5,11 +5,3 @@ const fileController = require('../controllers/fileController');
 router.post('/upload', fileController.uploadFile, fileController.handleFileUpload);
 
 module.exports = router;
-
-const express = require('express');
-const router = express.Router();
-const fileController = require('../controllers/fileController');
-
-router.post('/upload', fileController.uploadFile, fileController.handleFileUpload);
-
-module.exports = router;
